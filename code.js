a1b304854f4f36ba77afb9cb77bbf325d8da52c8
João Henrique <125095098+RochaJoaos@users.noreply.github.com>
2023-02-27 18:17:21 -0300

    update

---
+++ code.js
@@ -0,0 +1,7 @@
+function githubLink(){
+    window.open("https://github.com/RochaJoaos");
+}
+function linkedinLink(){
+    window.open("https://www.linkedin.com/in/jo%C3%A3o-henrique-s-b962a922b/");
+}
+

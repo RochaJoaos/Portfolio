11e95ce90a2098c4cd36721f1aaf955dccec65c7
João Henrique <125095098+RochaJoaos@users.noreply.github.com>
2023-03-10 20:05:24 -0300

    completed
    o projeto foi completo e só basta adicionar os sites criados posteriormente

--- code.js
+++ code.js
@@ -1,7 +1,10 @@
 function githubLink(){
     window.open("https://github.com/RochaJoaos");
 }
+
 function linkedinLink(){
     window.open("https://www.linkedin.com/in/jo%C3%A3o-henrique-s-b962a922b/");
 }
+
+
 
